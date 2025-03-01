72c5a86b75dff96e6485465a15e4d600cf328e3c
s-gala <galinasavtchenko78@gmail.com>
2025-03-01 16:35:50 +0300

    step 4

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/s-gala/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/s-gala/python-project-50/actions)
+
+### Asciinema generate_diff
+[![asciicast]( https://asciinema.org/a/cpQk3T2v3jLt3NQ6K9d4IaJTh.svg )]( https://asciinema.org/a/cpQk3T2v3jLt3NQ6K9d4IaJTh )
--- gendiff/engine.py
+++ gendiff/engine.py
@@ -2,9 +2,22 @@ import json
 
 
 def generate_diff(path_to_file_json1, path_to_file_json2):
-    json_data1 = json.load(open(path_to_file_json1))
-    json_data2 = json.load(open(path_to_file_json2))
-    data1 = json.dumps(json_data1)
-    data2 = json.dumps(json_data2)
-    return data1, data2
+    data1 = json.load(open(path_to_file_json1))
+    data2 = json.load(open(path_to_file_json2))
+    data_sort1 = dict(sorted(data1.items()))
+    data_sort2 = dict(sorted(data2.items()))  
+    data_diff = {}
 
+    for key in sorted((data_sort1|data_sort2).keys()):
+        if key in data_sort1 and key not in data_sort2:
+            data_diff[f'-{key}'] = data_sort1[key]
+        if key not in data_sort1 and key in data_sort2:
+            data_diff[f'+{key}'] = data_sort2[key]
+        if key in data_sort1 and key in data_sort2:
+            if data_sort1[key] == data_sort2[key]:
+                data_diff[key] = data_sort1[key]
+            else:
+                data_diff[f'-{key}'] = data_sort1[key]
+                data_diff[f'+{key}'] = data_sort2[key]
+    print(str(data_diff))
+    return(str(data_diff))
